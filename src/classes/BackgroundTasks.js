0e75327a6fe3f948d18773b886b67c4a1cb8e952
Orlando Canchola <orcj17544@gmail.com>
2022-08-21 14:38:25 -0500

    BackgroundTasks cannot be executed at the same time

--- src/classes/BackgroundTasks.js
+++ src/classes/BackgroundTasks.js
@@ -11,14 +11,127 @@ import { DAYS_LEFT_YEAR_NOTIFY, API_KEY_here, FRECUENCY_HOURS } from './../const
 import { storageUser, storagePositions, storageNotifications } from './../storage/storage.js';
 
 class BackgroundTasks {
+	static executingStorePosition = false;
+	static executingUpdatePositions = false;
+	static executingDisplayNotification = false;
+	
+	//Store the position in local memory
+	static async storePosition(backgroundClass, ...arrayFn) {
+		if (this.executingStorePosition) {
+			return;
+		}
+
+		this.executingStorePosition = true;
+
+		console.log('storePosition started');
+
+		Geolocation.getCurrentPosition(
+			async function getPosition(position) {
+				const latitude = position.coords.latitude;
+				const longitude = position.coords.longitude;
+
+				console.log('New position: ' + latitude + ', ' + longitude);
+
+				storagePositions.set(Date.now().toString(), JSON.stringify({latitude: latitude, longitude: longitude}));
+
+				this.executingStorePosition = false;
+
+				console.log('storePosition finished');
+
+				//functions to execute after get the new position
+				if (arrayFn.length > 0) {
+					for (const fn of arrayFn) {
+						if (backgroundClass.hasOwnProperty(fn.name)) {
+							await fn.call(backgroundClass);
+						} else {
+							fn();
+						}
+					}
+				}
+			},
+			(error) => {
+				// See error code charts below.
+				console.log(error.code, error.message);
+			},
+			{ timeout: (FRECUENCY_HOURS / 4) * 3.6 * Math.pow(10, 6), enableHighAccuracy: true, maximumAge: 1, distanceFilter: 1 }
+		);
+	}
+
+	//Update the country of the position and store it in memory
+	static async updatePositions() {
+		if (this.executingUpdatePositions) {
+			return;
+		}
+
+		this.executingUpdatePositions = true;
+
+		console.log('updatePositions started');
+
+		const state = await NetInfo.fetch();
+		let millisecondsDatesKeys = storagePositions.getAllKeys();
+
+		if (!state.isInternetReachable || millisecondsDatesKeys.length < 2) {
+			this.executingUpdatePositions = false;
+			console.log('updatePositions did not meet the requirements');
+			return;
+		}
+
+		let millisecondsDatesKeysIntegers = millisecondsDatesKeys.map(millisecondsDateKey => {
+			return parseInt(millisecondsDateKey, 10);
+		});
+
+		// Ascending order
+		millisecondsDatesKeysIntegers.sort(function(a, b){return a-b});
+
+		console.log('Milliseconds to update: ' + millisecondsDatesKeysIntegers);
+
+		for (var i = 0; i < millisecondsDatesKeysIntegers.length - 1; i++) {
+			const timeElapsedMilliseconds = millisecondsDatesKeysIntegers[i + 1] - millisecondsDatesKeysIntegers[i];
+			const timeElapsedDays = timeElapsedMilliseconds * 1.15741 * Math.pow(10, -8);
+			const position = JSON.parse(storagePositions.getString(millisecondsDatesKeysIntegers[i].toString()));
+
+			const coordinates = position.latitude + ',' + position.longitude;
+			const coordinatesEncoded = encodeURIComponent(coordinates);
+
+			const URL = 'https://revgeocode.search.hereapi.com/v1/revgeocode?' + 'at=' + coordinatesEncoded + '&' + 'lang=en-US' + '&' + 'apiKey=' + API_KEY_here;
+
+			const response = await axios.get(URL);
+
+			const countryCode = response.data.items[0].address.countryCode;
+
+			const userDataItem = JSON.parse(storageUser.getString(countryCode));
+
+			const newDays = userDataItem.days + timeElapsedDays;
+
+			console.log({...userDataItem, days: newDays});
+			storageUser.set(countryCode, JSON.stringify({...userDataItem, days: newDays, lastUpdate: Date.now()}));
+
+			storagePositions.delete(millisecondsDatesKeysIntegers[i].toString());
+		}
+
+		this.executingUpdatePositions = false;  
+
+		console.log('updatePositions finished');
+	};
+	
 	//Display the notification (background or foreground)
 	static async displayNotification() {
+		if (this.executingDisplayNotification) {
+			return;
+		}
+
+		this.executingDisplayNotification = true;
+
+		console.log('displayNotification started');
+
 		const daysLeftYear = this.getDaysLeftYear();
 
 		const goals = UserData.getGoals();
 		const latest = UserData.getLatest();
 
 		if (daysLeftYear > DAYS_LEFT_YEAR_NOTIFY) {
+			this.executingDisplayNotification = false;  
+			console.log('displayNotification did not meet the requirements');
 			return;
 		}
 
@@ -70,81 +183,13 @@ class BackgroundTasks {
 				sound: 'default', // Default sound for iOS
 			},
 		});
+
+		console.log('Id of notification: ' + latest.countryCode);
+
+		this.executingDisplayNotification = false;
+
+		console.log('displayNotification finished');
 	}
-
-	//Store the position in local memory
-	static async storePosition(backgroundClass, ...arrayFn) {
-		Geolocation.getCurrentPosition(
-			async function getPosition(position) {
-				const latitude = position.coords.latitude;
-				const longitude = position.coords.longitude;
-
-				console.log('Posición: ' + latitude + ', ' + longitude);
-
-				storagePositions.set(Date.now().toString(), JSON.stringify({latitude: latitude, longitude: longitude}));
-
-				if (arrayFn.length > 0) {
-					for (const fn of arrayFn) {
-						if (backgroundClass.hasOwnProperty(fn.name)) {
-							await fn.call(backgroundClass);
-						} else {
-							fn();
-						}
-					}
-				}
-			},
-			(error) => {
-				// See error code charts below.
-				console.log(error.code, error.message);
-			},
-			{ timeout: (FRECUENCY_HOURS / 4) * 3.6 * Math.pow(10, 6), enableHighAccuracy: true, maximumAge: 1, distanceFilter: 1 }
-		);
-	}
-	
-	//Update the country of the position and store it in memory
-	static async updatePositions() {
-		const state = await NetInfo.fetch();
-		let millisecondsDatesKeys = storagePositions.getAllKeys();
-
-		console.log('Sin organizar: ' + millisecondsDatesKeys);
-
-		if (!state.isInternetReachable || millisecondsDatesKeys.length < 2) {
-			return;
-		}
-
-		let millisecondsDatesKeysIntegers = millisecondsDatesKeys.map(millisecondsDateKey => {
-			return parseInt(millisecondsDateKey, 10);
-		});
-
-		// Ascending order
-		millisecondsDatesKeysIntegers.sort(function(a, b){return a-b});
-
-		console.log('Organizados: ' + millisecondsDatesKeysIntegers);
-
-		for (var i = 0; i < millisecondsDatesKeysIntegers.length - 1; i++) {
-			const timeElapsedMilliseconds = millisecondsDatesKeysIntegers[i + 1] - millisecondsDatesKeysIntegers[i];
-			const timeElapsedDays = timeElapsedMilliseconds * 1.15741 * Math.pow(10, -8);
-			const position = JSON.parse(storagePositions.getString(millisecondsDatesKeysIntegers[i].toString()));
-
-			const coordinates = position.latitude + ',' + position.longitude;
-			const coordinatesEncoded = encodeURIComponent(coordinates);
-
-			const URL = 'https://revgeocode.search.hereapi.com/v1/revgeocode?' + 'at=' + coordinatesEncoded + '&' + 'lang=en-US' + '&' + 'apiKey=' + API_KEY_here;
-
-			const response = await axios.get(URL);
-
-			const countryCode = response.data.items[0].address.countryCode;
-
-			const userDataItem = JSON.parse(storageUser.getString(countryCode));
-
-			const newDays = userDataItem.days + timeElapsedDays;
-
-			console.log({...userDataItem, days: newDays});
-			storageUser.set(countryCode, JSON.stringify({...userDataItem, days: newDays, lastUpdate: Date.now()}));
-
-			storagePositions.delete(millisecondsDatesKeysIntegers[i].toString());
-		}
-	};
 
 	static getDaysLeftYear() {
 		const currentDate = new Date();
