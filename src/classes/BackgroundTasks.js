0f9d363bc4e88f22632fbc4585065e06f066ac0f
Orlando Canchola Jimenez <orcj17544@gmail.com>
2022-08-22 14:40:23 -0500

    Fixed when storePosition and updatePositions get an error

--- src/classes/BackgroundTasks.js
+++ src/classes/BackgroundTasks.js
@@ -14,7 +14,7 @@ class BackgroundTasks {
 	static executingStorePosition = false;
 	static executingUpdatePositions = false;
 	static executingDisplayNotification = false;
-	
+
 	//Store the position in local memory
 	static async storePosition(backgroundClass, ...arrayFn) {
 		if (this.executingStorePosition) {
@@ -49,13 +49,18 @@ class BackgroundTasks {
 			}
 		}
 
-		const getPositionBound = getPosition.bind(this);
+		function getError(error) {
+			this.executingStorePosition = false;
+			// See error code charts below.
+			console.log(error.code, error.message);
+		}
 
-		Geolocation.getCurrentPosition(getPositionBound, 
-			(error) => {
-				// See error code charts below.
-				console.log(error.code, error.message);
-			},
+		const getPositionBound = getPosition.bind(this);
+		const getErrorBound = getError.bind(this);
+
+		Geolocation.getCurrentPosition(
+			getPositionBound, 
+			getErrorBound, 
 			{ timeout: (FRECUENCY_HOURS / 4) * 3.6 * Math.pow(10, 6), enableHighAccuracy: true, maximumAge: 1, distanceFilter: 1 }
 		);
 	}
@@ -97,26 +102,31 @@ class BackgroundTasks {
 			const coordinatesEncoded = encodeURIComponent(coordinates);
 
 			const URL = 'https://revgeocode.search.hereapi.com/v1/revgeocode?' + 'at=' + coordinatesEncoded + '&' + 'lang=en-US' + '&' + 'apiKey=' + API_KEY_here;
+			try {
+				const response = await axios.get(URL);
 
-			const response = await axios.get(URL);
+				const countryCode = response.data.items[0].address.countryCode;
 
-			const countryCode = response.data.items[0].address.countryCode;
+				const userDataItem = JSON.parse(storageUser.getString(countryCode));
 
-			const userDataItem = JSON.parse(storageUser.getString(countryCode));
+				const newDays = userDataItem.days + timeElapsedDays;
 
-			const newDays = userDataItem.days + timeElapsedDays;
+				console.log({...userDataItem, days: newDays});
+				storageUser.set(countryCode, JSON.stringify({...userDataItem, days: newDays, lastUpdate: Date.now()}));
 
-			console.log({...userDataItem, days: newDays});
-			storageUser.set(countryCode, JSON.stringify({...userDataItem, days: newDays, lastUpdate: Date.now()}));
-
-			storagePositions.delete(millisecondsDatesKeysIntegers[i].toString());
+				storagePositions.delete(millisecondsDatesKeysIntegers[i].toString());
+			} catch (error) {
+				this.executingUpdatePositions = false;  
+				console.error(error);
+				return;
+			}
 		}
 
 		this.executingUpdatePositions = false;  
 
 		console.log('updatePositions finished');
 	};
-	
+
 	//Display the notification (background or foreground)
 	static async displayNotification() {
 		if (this.executingDisplayNotification) {
